bddb244f94d9a2c31b1baf81cde9ce71c22b011c
Jeff Astor <jastor@laalliance.org>
2017-05-04 05:26:50 +0000

    Almost ready uber lab. Need readme.

---
+++ uber_methods.rb
@@ -0,0 +1,16 @@
+# Woah! Uber was just hacked and their code based deleted by a group attempting to replace them and take over the ride-sharing market.
+
+# Without hesitation they call Upperline School of Code and need some methods re-written that can calculate how much certain rides should cost.
+
+# Challenge #1 - Write a method for uberX that takes in 3 arguments: distance (in miles), time (in minutes), and surge pricing. Set the default for surge pricing to 1. Make sure it lets the user know exactly how much their fare is going to cost in a sentence that interpolates variables into a ruby string.
+
+# Take into consideration - http://uberestimator.com/cities/new-york
+
+# Base fare
+# Cost per minute
+# Cost per mile
+# Minimum charge for an Uber ride
+
+# uberX(2, 1) # Should come out to $5
+
+# Challenge #2 - Write another method that takes in an additional argument to specify if the ride is uberX, uberPool, uberXL, or uberBlack. Use a switch statement to change the fares depending on the string entered in for this value.
